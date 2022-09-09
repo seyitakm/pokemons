5848ee0cd6369d587f88fdee7110b3c53f679124
seyitakm <seyit200020@gmail.com>
2022-09-09 22:27:40 +0600

    m

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const API = "https://pokeapi.co/api/v2/pokemon/";
 
 let div = document.querySelector("#card");
+let modal = document.createElement("div");
+modal.classList.add("modal");
 
 async function cardFunction() {
   await fetch(`${API}`)
@@ -12,13 +14,57 @@ async function cardFunction() {
           .then((data) => {
             let card = document.createElement("div");
             card.classList.add("card");
-            let cardImg = document.createElement("div");
             card.innerHTML = data.name;
+            let cardImg = document.createElement("div");
             cardImg.innerHTML = `<img src=${data.sprites.front_default}>`;
             div.append(card);
-            div.append(cardImg);
+            card.append(cardImg);
+            card.addEventListener("click", (data) => {
+              modalWindow(data);
+              document.body.append(modal);
+            });
           });
       });
     });
 }
 cardFunction();
+// async function modalWindow(data) {
+//   await fetch(`${API}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       data.results.forEach((e) => {
+//         fetch(e.url)
+//           .then((res) => res.json())
+//           .then((data) => {
+//             let card1 = document.createElement("div");
+//             card1.classList.add("card");
+//             card1.innerHTML = data.name;
+//             let cardImg1 = document.createElement("div");
+//             cardImg1.innerHTML = `<img src=${data.sprites.front_default}>`;
+//             modal.append(card1);
+//             card1.append(cardImg1);
+//             document.body.append(modal);
+//           });
+//       });
+//     });
+// }
+function modalWindow(data) {
+  return `
+    <tr>
+    <td>Name: ${data.name}</td> 
+    </tr><br />
+    <tr>
+    <td>Weight: ${data.weight}</td>
+    </tr><br />
+    <tr>
+    <tr>
+    
+    </tr><br />
+    <tr>
+    <td>Growth: ${data.height}</td>
+    </tr><br />
+    <tr>
+    <td><img src=${data.sprites.front_default}></td>
+    </tr>
+    `;
+}
